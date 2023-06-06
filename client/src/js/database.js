32090bf58214b66cf18fccd07e2285ff9a8566f4
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-06-06 14:15:16 -0400

    Add functions and testing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Put to the database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req = store.put({ jate: content });
+  const res = await req;
+  console.log('🚀 Successfully saved data', res);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Get from the database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+  console.log('res.value', res);
+  return res;
+};
 
 initdb();
